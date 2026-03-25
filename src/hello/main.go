79cb5fabdb6ed9ea8fac1d427725e4f01370e04f
Yuan <whywhy36@gmail.com>
2026-03-25 21:03:04 +1300

    Re-implemented Go application to print 'Hello, World!'

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,8 +1,8 @@
-// Go Hello Crafting Application
+// Go Hello World Re-implementation
 package main
 
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Crafting")
+	fmt.Println("Hello, World!")
 }
