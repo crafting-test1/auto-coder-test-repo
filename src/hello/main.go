f874d312c7b12d14d1951db7a1686ad52b00865c
Yuan Zhuang <yuan@crafting.dev>
2026-04-01 10:23:58 +0000

    Re-implement Golang application as per DEV-4 instructions

--- src/hello/main.go
+++ src/hello/main.go
@@ -1,8 +1,24 @@
-// Go Hello Crafting Application
+// Go Crafting Application Re-implementation
 package main
 
 import "fmt"
 
+// Greeting represents a custom message to be displayed.
+type Greeting struct {
+	Message string
+}
+
+// NewGreeting creates a new Greeting with the given message.
+func NewGreeting(message string) Greeting {
+	return Greeting{Message: message}
+}
+
+// Display prints the Greeting's message to the console.
+func (g Greeting) Display() {
+	fmt.Println(g.Message)
+}
+
 func main() {
-	fmt.Println("Hello Crafting")
+	greeting := NewGreeting("Hello, Crafting World!")
+	greeting.Display()
 }
